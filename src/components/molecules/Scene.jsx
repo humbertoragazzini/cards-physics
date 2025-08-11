4ff032954530ff1426438f075323061d800c27d4
humbertoragazzini <beto_vicio@hotmail.com>
2025-08-11 19:35:52 +0100

    removed unused stuff

--- src/components/molecules/Scene.jsx
+++ src/components/molecules/Scene.jsx
@@ -1,18 +1,32 @@
+import { Html } from "@react-three/drei";
 import Effects from "../atoms/Effects";
 import HtmlObj from "../atoms/HtmlObj";
 import TheBox from "../atoms/TheBox";
 import TheFloor from "../atoms/TheFloor";
+import Card from "../atoms/Card";
 export default function Scene() {
   return (
     <group>
-      <Effects>
+      {/* <Effects>
         {(texture) => {
           return (
             <group>
-              <TheBox size={{x:1,y:1,z:1}} envMap={texture} name={"new-box-1"} position={[0,0,0]} rotation={[0,0,0]}></TheBox>
+              <TheBox size={{ x: 1, y: 1, z: 1 }} envMap={texture} name={"new-box-1"} position={[0, 0, 0]} rotation={[0, 0, 0]}></TheBox>
             </group>)
         }}
-      </Effects>
+      </Effects> */}
+      
+      <Card
+        card={"/cards2-2.glb"}
+        position={{ x: 0, y: 0, z: 0 }}
+        rotationZ={Math.PI / 25}
+        thisFirst={1 == 0}
+        translate={1}
+        selected={false}
+        setSelected={() => { }}
+      >
+      </Card>
+      <ambientLight intensity={50}></ambientLight>
       {/* <HtmlObj occlude={true} position={[0, 0, -11.5]} name={"new-html-2"}>
         <div className="overflow-hidden h-screen w-screen border-2 border-black bg-black">
           <p className="text-base text-black">This is a div</p>
