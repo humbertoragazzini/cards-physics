f5c3658c7d045e27edf77b6dda58bb31b83bb258
humbertoragazzini <beto_vicio@hotmail.com>
2025-08-11 19:49:02 +0100

    added physics to the card

--- src/components/molecules/Scene.jsx
+++ src/components/molecules/Scene.jsx
@@ -1,35 +1,36 @@
 import Card from "../atoms/Card";
-import {Physics} from "@react-three/rapier"
+import { Physics, RigidBody } from "@react-three/rapier"
 export default function Scene() {
   return (
     <group>
       <Physics>
 
-      <Card
-        card={"/cards2-2.glb"}
-        position={{ x: 0, y: 0, z: 0 }}
-        rotationZ={0}
-        thisFirst={1 == 0}
-        translate={1}
-        selected={false}
-        setSelected={() => { }}
-      >
-      </Card>
-      
+      <RigidBody>
+        <Card
+          card={"/cards2-2.glb"}
+          position={{ x: 0, y: 0, z: 0 }}
+          rotationZ={0}
+          thisFirst={1 == 0}
+          translate={1}
+          selected={false}
+          setSelected={() => { }}
+        >
+        </Card>
+      </RigidBody>
+      <
       <Floor></Floor>
 
       </Physics>
-      
       <ambientLight intensity={50}></ambientLight>
     </group>
   );
 }
 
 
-function Floor(){
-  return(
-    <mesh position={[0,-2,0]}>
-      <boxGeometry args={[10,0.2,10]}></boxGeometry>
+function Floor() {
+  return (
+    <mesh position={[0, -2, 0]}>
+      <boxGeometry args={[10, 0.2, 10]}></boxGeometry>
       <meshBasicMaterial color={"white"}></meshBasicMaterial>
     </mesh>
   )
